0b19bf80063618867c93c15c03d7a0846c0cc4ed
LieutLet <thesmartalec155@gmail.com>
2025-04-13 01:48:54 -0400

    Added the default constructor, normal constructor, and two functions that add and removed the files that the process needs.

--- PCB.h
+++ PCB.h
@@ -9,6 +9,9 @@
 //*********************************************************************//
 
 //#include <unistd.h>
+#include <vector>
+#include <string>
+#include <cstdio>
 
 using namespace std;
 
@@ -27,18 +30,83 @@ public:
 	int getStackPointer() const { return stackPointer; }
 	int getState() const { return state; }
 	int getOpenFiles() const { return openFiles; }
+	//int getRunTime() const { return runTime; }
 	// Setters
 	void setMemoryLimit(int limit) { memoryLimit = limit; }
-	void setState(int state) { state = newState; }
-	void setOpenFiles(int* openFiles) { this->openFiles = openFiles; }
+	void setState(int newState) { state = newState; }
+	int addOpenFiles(string file);
+	//void setRunTime(int time) { runTime = time; }
+
+	// Other methods
+	int removeOpenFiles(string file);		// Remove file from open files
 
 private:
 	int pid;								// Process ID
 	int stackPointer;						// Stack pointer
 	int state;								// Process state (running, ready, waiting)
 	int memoryLimit;						// Memory limit
-	int* openFiles[memoryLimit];			// List of open files
+	vector<string>openFiles;				// List of open files
+	//int runTime;							// Run time
 
+};
+
+//Default constructor
+PCB::PCB()
+{
+	pid = 0;
+	stackPointer = 0;
+	state = 0;								// Default state
+	memoryLimit = 0;						// Default memory limit
+	//runTime = 0;							// Default run time
 }
 
+// Constructor
+PCB::PCB(int pid, int stackPointer)
+{
+	pid = pid;
+	stackPointer = stackPointer;
+	state = 0;								// Default state
+	memoryLimit = 0;						// Default memory limit
+	//runTime = 0;							// Default run time
+}
+
+// *****************************************************************//
+// Function to add files to the list of open files.				    //
+// Takes the pathname of the file as a parameter.					//
+// If there is space in the list, the file is added, and 1 is		//
+// returned. If there is no space, 0 is returned.					//
+// *****************************************************************//
+int PCB::addOpenFiles(string file);
+{
+	if (openFiles.size() <= memoryLimit) {
+		openFiles.append(file);					// Add file to open files
+		return 1;								// File added successfully
+	}
+	else {
+		perror("Error: Memory limit exceeded");
+		return 0;								// File not added
+	}
+}
+
+// *****************************************************************//
+// A function to remove a specific file from the list of open files //
+// returns 0 if the file could not be removed, 1 if it was.		    //
+// *****************************************************************//
+int PCB::removeOpenFiles(string file);
+{
+	if (openFiles.empty() == true) {
+		perror("Error: No open files");
+		return 0;
+	}
+	for (int i = 0; i < openFiles.size(); i++) {
+		if (openFiles[i] == file) {
+			openFiles.erase(openFiles.begin() + i);	// Remove file from open files
+			return 1;
+		}
+		else {
+			perror("Error: File not found");
+			return 0;
+		}
+	}
+}
 #endif // !PCB
